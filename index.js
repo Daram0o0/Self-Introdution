800adadb58cfc30b332486913a13cef513233cdf
Daram0o0 <c30728535@gmail.com>
2024-04-13 22:50:22 +0900

    skill section animation 실패 및 최종본

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+// Nav(Header) Navigation
+let aTags = document.querySelectorAll('header a');
+for (var i = 0; i < aTags.length; i++) {
+  aTags[i].onclick = function (e) {
+    e.preventDefault();
+    let target = document.querySelector(this.getAttribute('href'));
+
+    window.scrollTo({
+      behavior: 'smooth',
+      top: target.offsetTop,
+    });
+  };
+}
+
+// Go btn Navigation
+document.querySelector('.btn').addEventListener('click', function () {
+  let target = document.querySelector('#me');
+
+  window.scrollTo({
+    behavior: 'smooth',
+    top: target.offsetTop,
+  });
+});
+
+
 // section2 => me
 const info = {
   "Location": "Ulsan, Korea",
@@ -29,3 +54,7 @@ function changeWord(element, key) {
 function restoreWord(element, key) {
   element.innerText = key
 }
+
+// skill section animation
+// 원래 section("#skill")에 도착하면 애니메이션이 사작되게 하고 싶었으나, 하지 못함
+
